eff72571981bd17e53a1a56f0ca5333a99a40869
zhou-you <zhouyou1989123@gmail.com>
2017-04-07 17:37:27 +0800

    修改了解码

--- app/src/main/java/com/zhouyou/audio/demo/decoder/FSKDecoderData.java
+++ app/src/main/java/com/zhouyou/audio/demo/decoder/FSKDecoderData.java
@@ -3,12 +3,8 @@ package com.zhouyou.audio.demo.decoder;
 import com.zhouyou.audio.decoder.AbstractDecoder;
 import com.zhouyou.audio.decoder.DecoderResult;
 import com.zhouyou.audio.util.AudioLog;
-import com.zhouyou.audio.util.CRCUtil;
-import com.zhouyou.audio.util.Tools;
 
 import org.jtransforms.fft.DoubleFFT_1D;
-
-import java.util.Arrays;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -21,17 +17,7 @@ import rx.functions.Action1;
  * 版本： v2.0<br>
  */
 public class FSKDecoderData extends AbstractDecoder {
-    private short mnMax = 32767;
     private int sampleRate;
-    private int bitLen = 78;
-    private int mCacheLen = 18990;//10998 14582 14336 华为p6的minbuf是7680
-    private int mMaxIndex = 0;
-    private short[] mCacheBuffer = new short[mCacheLen];
-    private int mCaptureLen = 0;
-    private int mCaptureState = 0;        // 状态ֹ
-    // 在指定误差范围内检测
-    private final int nDetection[] = {142, 141, 140, 139, 138, 137, 143, 144, 145};
-    private byte[] dataBytes;
 
     @Override
     public void start() {
@@ -53,55 +39,7 @@ public class FSKDecoderData extends AbstractDecoder {
 
     @Override
     public void decoderData(short[] audio_data, int bufferReadResult) throws Exception {
-        //找出信号中符合条件的最大值
-        int mMaxLimit = 7000;
-        int mMax = 0;
-        if (mCaptureState == 0) {
-            for (int i = 0; i < bufferReadResult - 20; i += 1) {
-                mMax = (short) Math.max(mMax, Math.abs(audio_data[i]));
-                if (mMax >= mnMax || mMax > 1000) {
-                    int tempcount = 0;
-                    for (int j = 0; j < 12; j++) {
-                        if (Math.abs(audio_data[i + j]) > mMaxLimit) tempcount++;
-                    }
-                    if (tempcount > 8) {
-                        mMaxIndex = i;
-                        mCaptureState = 1;
-                        break;
-                    }
-                }
-            }
-        }
-
-        //找到起始点填充buffer缓存
-        if (mCaptureState == 1) {
-            int nCopyLen = Math.min(bufferReadResult, mCacheLen - mCaptureLen);
-            System.arraycopy(audio_data, 0, mCacheBuffer, mCaptureLen, nCopyLen);
-            mCaptureLen += nCopyLen;
-        }
-        boolean bOk = false;
-        if (mCaptureLen >= mCacheLen) {
-            mCaptureState = 2;
-            long mTime = System.currentTimeMillis();
-            //print("mMaxIndex:"+mMaxIndex+" ,bufferReadResult"+bufferReadResult);
-            for (int i = 0; i < nDetection.length; i++) {
-                int nFrameLen = nDetection[i];
-                bOk = transform(mCacheBuffer, mMaxIndex, nFrameLen);
-                if (bOk) {
-                    print("执行一次分析 >> 成功" + nFrameLen);
-                    decoderDataBySuccess(dataBytes);
-                    break;
-                } else {
-                    print("执行一次分析 >> 失败" + nFrameLen);
-                }
-            }
-            print(">>>>>>>>>>>>>>>>" + (System.currentTimeMillis() - mTime));
-            if (!bOk) {
-                //decoderDataByFail(DecoderResult.REC_ERR,"decoder err!!!");
-            }
-            mCaptureState = 0;
-            mCaptureLen = 0;
-        }
+        //具体的解码，涉及到业务具体通信相关已经取消
     }
 
     /**
@@ -144,80 +82,8 @@ public class FSKDecoderData extends AbstractDecoder {
 
     @Override
     public void recoderState(boolean isPause) {
-        mCaptureState = 0;
-        mCaptureLen = 0;
     }
-
-    // 解释数据  先检查头尾为了提高效率
-    public boolean transform(short[] buffer, int mMaxIndex, int nFrameLen) {
-        // 数字变换
-        final int nBits[] = new int[bitLen];
-        short parityBit = 0;//求出校验的数据位和
-        short[] shorts = new short[nFrameLen];
-        //检查头
-        int startLen = 4;
-        for (int i = 0; i < startLen; i++) {
-            System.arraycopy(buffer, mMaxIndex + i * nFrameLen, shorts, 0, nFrameLen);
-            int maxindex = calculateFFT(shorts);
-            int nBit = getBit(maxindex);
-            if (nBit == -1) return false;
-            nBits[i] = nBit;
-            parityBit += nBit;
-        }
-        if (nBits[0] != 1 || nBits[1] != 0 || nBits[2] != 1 || nBits[3] != 0) {
-            print(">>>>头部不正确！！！");
-            return false;
-        }
-
-        //检查尾
-        System.arraycopy(buffer, mMaxIndex + (bitLen - 1) * nFrameLen, shorts, 0, nFrameLen);
-        int maxindex = calculateFFT(shorts);
-        int endBit = getBit(maxindex);
-        if (endBit == -1) return false;
-        if (endBit != 1) {
-            print(">>>>尾部不正确！！！");
-            return false;
-        }
-        nBits[bitLen - 1] = endBit;
-
-        //检查中间
-        for (int i = startLen; i < bitLen - 1; i++) {
-            System.arraycopy(buffer, mMaxIndex + i * nFrameLen, shorts, 0, nFrameLen);
-            int index = calculateFFT(shorts);
-            int nBit = getBit(index);
-            if (nBit == -1) return false;
-            nBits[i] = nBit;
-            if (i < bitLen - 2) {
-                parityBit += nBit;
-            }
-        }
-
-        final String result = Arrays.toString(nBits).replaceAll("[\\[\\]\\s,]", "");
-        print("result：" + result);
-        //print("make validateBit：" + (parityBit & 0x01));
-        int validate = nBits[bitLen - 2];
-        if ((parityBit & 0x01) != validate) {
-            print(">>>>奇偶校验失败！");
-            return false;
-        }
-        final int bytelen = (bitLen - 6) / 8;
-        dataBytes = new byte[bytelen];
-        for (int i = 0; i < bytelen; i++) {
-            byte dataRxByte = 0;
-            for (int k = 0; k < 8; k++) {
-                dataRxByte |= (nBits[4 + i * 8 + k] << (8 - k - 1));
-            }
-            dataBytes[i] = dataRxByte;
-        }
-        byte crc = CRCUtil.calcCrc8(dataBytes, 0, dataBytes.length - 1);
-        print("data:" + Tools.byte2Hex(dataBytes));
-        if (crc != dataBytes[dataBytes.length - 1]) {
-            print(">>>>CRC校验失败:" + Tools.byte2Hex(crc) + " : " + Tools.byte2Hex(dataBytes[dataBytes.length - 1]));
-            return false;
-        }
-        return true;
-    }
-
+    
     /**
      * 傅里叶变化从原始信号中获取频率信息
      * @param signal
---
+++ app/src/main/java/com/zhouyou/audio/demo/encoder/EncoderData1.java
@@ -0,0 +1,138 @@
+package com.zhouyou.audio.demo.encoder;
+
+import com.zhouyou.audio.track.RxAudioPlayer;
+import com.zhouyou.audio.util.AudioLog;
+import com.zhouyou.audio.util.SinWave;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+/**
+ * <p>描述：实现发送的数据组装</p>
+ * 1.将byte数据转换为硬件协议需要的15位二进制01，起始码（4）+数据长度8位（8）<br>
+ * 2.发送一个字节按照协议编码后对应的二进制01<br>
+ * 3.把二进制的01转化成频率的正玄波数组<br>
+ * 4.间歇发送数据
+ * 作者： ~若相惜<br>
+ * 日期： 2017/2/9 13:39<br>
+ * 版本： v2.0<br>
+ */
+public class EncoderData1 implements RxAudioPlayer.IAudioEncoder {
+    private static final String START_CODE = "1010";//起始码
+    private static final String END_CODE = "01";//结束码
+    private static final int bit = 8;//数据长度8位
+    private static final int validate = 1;//校验码1位
+    private static final int all_len = START_CODE.length() + bit + validate + END_CODE.length();//硬件发送数据最长是15
+    private static final int HIGH_FREQ = 2000;
+    private static final int LOW_FREQ = 1000;
+    private static final int duration = 3;//3ms
+    private static final int INTERIM = 10;//发送两个字节之间间歇的时间 duration*INTERIM
+    private static byte[] interim_buf;
+    private int audioBufLength;
+    private int duration_len;
+    private int sampleRate;
+    private ByteBuffer mByteDataBuf;
+    private byte[] LOW_FREQ_BYTE;
+    private byte[] HIGH_FREQ_BYTE;
+
+    @Override
+    public void startEncoder(int sampleRate) {
+        this.sampleRate = sampleRate;
+        duration_len = duration * sampleRate / 1000;
+        audioBufLength = duration_len * 2 * all_len;
+        interim_buf = new byte[duration_len * INTERIM];
+        LOW_FREQ_BYTE = SinWave.sin(LOW_FREQ, this.sampleRate, duration_len);
+        HIGH_FREQ_BYTE = SinWave.sin(HIGH_FREQ, this.sampleRate, duration_len);
+    }
+
+    @Override
+    public byte[] encoderData(byte[] bytes) {
+        mByteDataBuf = ByteBuffer.allocate(audioBufLength * bytes.length + interim_buf.length * bytes.length);
+        for (byte aByte : bytes) {
+            byte[] single_wave_all = bitToFreqWave(aByte);
+            mByteDataBuf.put(single_wave_all);
+            mByteDataBuf.put(interim_buf);//间隔10ms发送数据，这里采用的办法是用0填充
+        }
+        return mByteDataBuf.array();
+    }
+
+    /**
+     * 把二进制的01转化成频率的正玄波数组
+     *
+     * @param data
+     * @return
+     */
+    private byte[] bitToFreqWave(byte data) {
+        final short[] audioBit = bitShort(data);
+        final String strAlldata = Arrays.toString(audioBit).replaceAll("[\\[\\]\\s,]", "");
+        print("strAlldata:" + strAlldata);
+        ByteBuffer mByteDataBuf = ByteBuffer.allocate(audioBufLength);
+        byte[] waveLen = new byte[duration_len * 2];
+        for (short i : audioBit) {
+            switch (i) {
+                case 0:
+                    waveLen = LOW_FREQ_BYTE;
+                    break;
+                case 1:
+                    waveLen = HIGH_FREQ_BYTE;
+                    break;
+            }
+            mByteDataBuf.put(waveLen);
+        }
+        mByteDataBuf.flip();
+        int all_length = mByteDataBuf.limit();
+        byte[] wave_all = new byte[all_length];
+        mByteDataBuf.get(wave_all);
+        return wave_all;
+    }
+
+    @Override
+    public void finishEncoder() {
+        mByteDataBuf = null;
+    }
+
+    /**
+     * 发送一个字节按照协议编码后对应的二进制01
+     *
+     * @param audioData 字节数据
+     * @return 返回编码后对应的二进制数组
+     */
+    private short[] bitShort(byte audioData) {
+        short[] audioBit = new short[all_len];
+        byte paritybit = 0;//校验值
+        //填写起始位
+        for (int i = 0; i < START_CODE.length(); i++) {//起始码
+            audioBit[i] = Short.valueOf(START_CODE.substring(i, i + 1));
+            paritybit += audioBit[i];
+        }
+        int dataStartIndex = START_CODE.length();
+        //填写数据位
+        final int srcDataLen = dataStartIndex + bit;
+        for (int counter_i = dataStartIndex; counter_i < srcDataLen; counter_i++) {
+            if (((audioData >> (srcDataLen - counter_i - 1) & 0x01) == 0x01))
+                audioBit[counter_i] = 1;
+            else
+                audioBit[counter_i] = 0;
+            paritybit += audioBit[counter_i];
+        }
+
+        print("paritybit:" + (paritybit & 0x01));
+        //填写校验位
+        if ((paritybit & 0x01) == 0x01) {
+            audioBit[dataStartIndex + bit] = 1;
+        } else {
+            audioBit[dataStartIndex + bit] = 0;
+        }
+        //填写结束位
+        int endIndex = dataStartIndex + bit + 1;
+        for (int i = endIndex; i < (endIndex + END_CODE.length()); i++) {//结束码
+            audioBit[i] = Short.valueOf(END_CODE.substring(i - endIndex, i - endIndex + 1));
+            paritybit += audioBit[i];
+        }
+        return audioBit;
+    }
+
+    private void print(String msg) {
+        AudioLog.i(msg);
+    }
+}
